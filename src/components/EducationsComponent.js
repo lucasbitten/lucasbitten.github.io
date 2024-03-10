5943a3fe89f2cc07e18e82e48843b40dcd508e21
Lucas Bittencourt <lucasbbitten@gmail.com>
2024-03-10 03:40:42 -0400

    Separating components

---
+++ src/components/EducationsComponent.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Container, ListGroup } from 'react-bootstrap';
+import SectionHeaderComponent from './SectionHeaderComponent';
+import ExperienceCardComponent from './ExperienceCardComponent';
+
+const EducationsComponent = () => {
+  const educationList = [
+    {
+      title: "Centennial College",
+      time: "2019 - 2021",
+      place: "Toronto, Canada",
+      companyName: "",
+      description: "Java Programming (Multithreading, Lambda, Collections, etc.) C# and ASP.Net Programming 3 (Lambda, Delegates, Collections, Multithreading, WPF, Entity Framework, etc.) Android Applications Development (Intents, Activities, Visual Components, etc) Assets for Game Programming (3D Modeling, 2D Sprite, Animations, etc) Game Programming (Finite State Machines (FSMs), Artificial Intelligence (AI), Particle Systems (PS), and Multi-Player Networked Games (MPNGs)) Practical Game Design (Design Integration; Control Schemes; Game Balancing; Gameplay Mechanics; Player Interaction)",
+      logoSrc: "https://media.licdn.com/dms/image/D560BAQGDFaZK89FUAQ/company-logo_100_100/0/1696248903527/centennial_college_logo?e=1717027200&v=beta&t=KkSJ7UsSovvZ3se-v0QEcw8HuKpfFBCtQfVzXym0yv4"
+    },
+    {
+      title: "Universidade Federal Fluminense",
+      time: "2011 - 2018",
+      place: "Rio de Janeiro, Brazil",
+      companyName: "",
+      description: "",
+      logoSrc: "LogoUff.png"
+    },
+  ];
+
+  return (
+    <Container fluid className="bg-section" id="education">
+      <Container className="container">
+        <SectionHeaderComponent title={'Education'} />
+        <ListGroup>
+          {educationList.map((education, index) => (
+            <ListGroup.Item key={index} className='experience-list-item'>
+              <ExperienceCardComponent {...education} />
+            </ListGroup.Item>
+          ))}
+        </ListGroup>
+      </Container>
+    </Container>
+  );
+};
+
+export default EducationsComponent;
---
+++ src/components/ProjectsComponent.js
@@ -0,0 +1,92 @@
+import Container from 'react-bootstrap/Container';
+import ProjectCardComponent from './ProjectCardComponent';
+import SectionHeaderComponent from './SectionHeaderComponent'
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+
+function ProjectsComponent() {
+    return (
+        <Container className="bg-section" id="projects">
+          <SectionHeaderComponent title={'Comercial Projects'}/>
+          <Row xs={1} md={2} lg={3}>
+            <Col className='column'>
+              <ProjectCardComponent
+                title="A(maze)ing Race"
+                companyName="Gameloft Toronto"
+                projectImage="https://t2.gstatic.com/licensed-image?q=tbn:ANd9GcRKrodWjRjr7pwTP_AMc9rmBK1NiRvMyjupMwjofTmnQLiK4cdBCoDTF1JkQYtsEpe3"
+                environment="Unity, Visual Studio, C#, C++"
+                description="Ported and adapted C++ code to Unity, fixed bugs, implemented features"
+                playLink="https://play.google.com/store/apps/details?id=com.gameloft.android.ANMP.GloftDYHM"
+                logoSrc="https://media.licdn.com/dms/image/D4E0BAQEmwyoTRo6i9w/company-logo_100_100/0/1703064670906/gameloft_logo?e=1717027200&amp;v=beta&amp;t=SmA9SRuEjksUxBFt0l-McQcbVIxy7_ETjOw0Al_oH-U"
+              />
+            </Col>
+
+            <Col className='column'>
+              <ProjectCardComponent
+                title="LEGO® Legacy: Heroes Unboxed"
+                companyName="Gameloft Toronto"
+                projectImage="https://play-lh.googleusercontent.com/pgxuatGUAdw1KHnWM_UKTYcGw8erFmhxY4WIwlGnFlIKMw5zzub_6nQPEXeszXGEzLk4=w240-h480-rw"
+                environment="C#, Visual Studio, Proprietary Engine"
+                description="Implemented features, fixed bugs"
+                playLink="https://apps.apple.com/us/app/lego-legacy-heroes-unboxed/id1644208767"
+                logoSrc="https://media.licdn.com/dms/image/D4E0BAQEmwyoTRo6i9w/company-logo_100_100/0/1703064670906/gameloft_logo?e=1717027200&amp;v=beta&amp;t=SmA9SRuEjksUxBFt0l-McQcbVIxy7_ETjOw0Al_oH-U"
+              />
+            </Col>
+
+
+            <Col className='column'>
+              <ProjectCardComponent
+                title="SongPop Party"
+                companyName="Gameloft Toronto"
+                projectImage="https://images.nintendolife.com/d1d7e92f2631b/songpop-party-cover.cover_large.jpg"
+                environment="Unity, Visual Studio, C#"
+                description="Fixed gameplay bugs"
+                playLink="https://songpop-party.com/"
+                logoSrc="https://media.licdn.com/dms/image/D4E0BAQEmwyoTRo6i9w/company-logo_100_100/0/1703064670906/gameloft_logo?e=1717027200&amp;v=beta&amp;t=SmA9SRuEjksUxBFt0l-McQcbVIxy7_ETjOw0Al_oH-U"
+              />
+            </Col>
+
+            <Col className='column'>
+              <ProjectCardComponent
+                title="Smash Ball"
+                companyName="Game Pill"
+                projectImage="https://media.moddb.com/images/games/1/75/74928/1024_thumb.jpg"
+                environment="Unity, Visual Studio, C#, C++"
+                description="Implemented gameplay functionalities and fixed bugs"
+                playLink="https://store.steampowered.com/app/905360/Smash_Ball/"
+                logoSrc="https://media.licdn.com/dms/image/D560BAQHhhVGSr4nKeA/company-logo_200_200/0/1684957962085/gamepillinc_logo?e=2147483647&v=beta&t=5uZu5pTSYpeEWFsS_sN6AYGRMU5Y-75gD6BUpTmlH3M"
+              />
+            </Col>
+
+            <Col className='column'>
+              <ProjectCardComponent
+                title="Unannounced project"
+                companyName="Gameloft Toronto"
+                projectImage="Question mark.png"
+                environment="Unity, Visual Studio, C#"
+                description="Implemented gameplay features to rapidly created prototype"
+                playLink=""
+                logoSrc="https://media.licdn.com/dms/image/D4E0BAQEmwyoTRo6i9w/company-logo_100_100/0/1703064670906/gameloft_logo?e=1717027200&amp;v=beta&amp;t=SmA9SRuEjksUxBFt0l-McQcbVIxy7_ETjOw0Al_oH-U"
+              />
+            </Col>
+
+
+            <Col className='column'>
+              <ProjectCardComponent
+                title="Unannounced project"
+                companyName="Gameloft Toronto"
+                projectImage="Question mark.png"
+                environment="Unity, Visual Studio, C#"
+                description="Implemented foundation systems and added gameplay functionalities"
+                playLink=""
+                logoSrc="https://media.licdn.com/dms/image/D4E0BAQEmwyoTRo6i9w/company-logo_100_100/0/1703064670906/gameloft_logo?e=1717027200&amp;v=beta&amp;t=SmA9SRuEjksUxBFt0l-McQcbVIxy7_ETjOw0Al_oH-U"
+              />
+            </Col>
+          </Row>
+
+
+        </Container>
+    );
+}
+
+export default ProjectsComponent;
